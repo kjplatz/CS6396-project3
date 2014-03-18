e1eb13210f7c51e2c625e85b258625f8ed15f6f6
Kenneth Platz <kplatz@utdallas.edu>
2014-03-18 16:39:21 -0500

    Initial revision

---
+++ src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.myfirstapp;
+
+import java.util.ArrayList;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+	
+	protected ArrayList<String> getBluetoothDevices() {
+		ArrayList<String> list = new ArrayList<String>();
+		
+		BluetoothAdapter BA;
+		BA = BluetoothAdapter.getDefaultAdapter();
+		
+		Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		startActivityForResult(turnOn, 0);
+		
+		return list;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+		final ListView listview = (ListView) findViewById(R.id.listview);
+		final ArrayList<String> list = new ArrayList<String>();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
